de910100bf4ae786e5890fdcf717cf789093f5b5
Thrauk <stoicath@gmail.com>
2021-05-18 17:08:22 +0300

    Added configuration for packaging

--- src/main/java/com/server/restservice/firebase/FirebaseInitialization.java
+++ src/main/java/com/server/restservice/firebase/FirebaseInitialization.java
@@ -3,21 +3,26 @@ package com.server.restservice.firebase;
 import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Service;
+import org.springframework.util.ResourceUtils;
 
 import javax.annotation.PostConstruct;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.InputStream;
 
 @Service
 public class FirebaseInitialization {
 
     @PostConstruct
-    public void initialization() throws FileNotFoundException {
+    public void initialization() throws IOException {
 
-        FileInputStream serviceAccount =
-                new FileInputStream("./serviceAccountKey.json");
+        InputStream serviceAccount =
+                //new FileInputStream("./serviceAccountKey.json");
+                //new FileInputStream(new ClassPathResource("serviceAccountKey.json").getFile());
+                getClass().getResourceAsStream("/serviceAccountKey.json");
 
         FirebaseOptions options = null;
         try {
